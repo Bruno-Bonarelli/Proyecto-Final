cae9910958d4f2b960a44e3b2c4b18370b732abd
bona <brunobonarelli@gmail.com>
2022-05-17 19:35:28 -0300

    V 1.0

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,98 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <CarritoDeCompras :productos="productosEnCarrito" />
+
+        <div class="contenedorProductos">
+            <ProductoVenta
+                attr1="attr1"
+                attr2="attr2"
+                v-bind="$attrs"
+                v-for="(item, i) in productos"
+                :key="i"
+                :id="item.id"
+                :titulo="item.titulo"
+                :precio="item.precio"
+                :nombre-imagen="item.nombreImagen"
+                :destacado="item.destacado"
+                @agregar-al-carrito="AgregarAlCarrito"
+            >
+            </ProductoVenta>
+        </div>
+
+        
+    </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProductoVenta from "./components/Productos-venta.vue";
+import CarritoDeCompras from "./components/Carrito-de-compras.vue";
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    components: {
+        ProductoVenta,
+        CarritoDeCompras,
+    },
+    data() {
+        return {
+            productos: [
+                {
+                    id: 1,
+                    titulo: "Cocacola 500ml",
+                    precio: 110,
+                    nombreImagen: "cocacola.jpg",
+                    destacado: false,
+                },
+                {
+                    id: 2,
+                    titulo: "Galletita Sonrisa",
+                    precio: 125,
+                    nombreImagen: "galletita-sonrisa.jpg",
+                    destacado: false,
+                },
+                {
+                    id: 3,
+                    titulo: "Alfajor Capital del espacio",
+                    precio: 150,
+                    nombreImagen: "alfajor-capitan-del-espacio.jpg",
+                    destacado: true,
+                },
+            ],
+            productosEnCarrito: [],
+        };
+    },
+    mounted() {},
+    methods: {
+        AgregarAlCarrito(id) {
+            let result = this.productos.find((prod) => {
+                return prod.id == id;
+            });
+            this.productosEnCarrito.push(result);
+            // if (result) {
+            //     this.$refs.miCarrito.AgregarProducto(result);
+            // }
+        },
+    },
+};
 </script>
 
+
 <style>
+body {
+    margin: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #111;
+}
+.contenedorProductos {
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 </style>
